076c6115dfac0bdbe93d2ad405b3e7b6dd2324eb
francescoferrini <51090995+francescoferrini@users.noreply.github.com>
2021-04-19 20:29:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -82,6 +82,7 @@ def extractSpanInfo(sentence):
 
 print("---------- Exercise1 ----------")
 
+
 example = ("The fire was caused by the gas stove and not by the gas tank.")
 print("The sentence is: ", example)
 
@@ -93,11 +94,68 @@ print("\n---------- Exercise2 ----------")
 
 print("The sentence is: ", example)
 
-dict = subtreeExtraction(example)
-print(dict)
+subtree_list = subtreeExtraction(example)
+for list in subtree_list:
+    print(list)
     
 print("\n---------- Exercise3 ----------")
 
+print("The sentence is: ", example)
+
+word_list =["The", "fire"]
+
+if checkSubtree(example, word_list) == True:
+    print("The word list", word_list, "forms a subtree")
+else:
+    print("The word list", word_list, "doesn't form a subtree")
+    
+print("\n---------- Exercise4 ----------")
+
+example2 = "Bills on ports and immigration were submitted by Brownback Republican Senator of Kansas"
+print("The sentence is: ", example2)
+
+doc = nlp(example2)
+span = doc[7:12]
+print("This is the span: ", span)
+
+span_head = spanHead(span)
+print("The head is: ", span_head)
+
+print("\n---------- Exercise5 ----------")
+
+example3 = "Carla bought an iphone and gave it to her boyfriend"
+print("The sentence is: ", example3)
+
+dict = extractSpanInfo(example3)
+print(dict)
+print("The sentence is: ", example)
+
+word_list =["The", "fire"]
+
+if checkSubtree(example, word_list) == True:
+    print("The word list", word_list, "forms a subtree")
+else:
+    print("The word list", word_list, "doesn't form a subtree")
+    
+print("\n---------- Exercise4 ----------")
+
+example2 = "Bills on ports and immigration were submitted by Brownback Republican Senator of Kansas"
+print("The sentence is: ", example2)
+
+doc = nlp(example2)
+span = doc[7:12]
+print("This is the span: ", span)
+
+span_head = spanHead(span)
+print("The head is: ", span_head)
+
+print("\n---------- Exercise5 ----------")
+
+example3 = "Carla bought an iphone and gave it to her boyfriend"
+print("The sentence is: ", example3)
+
+dict = extractSpanInfo(example3)
+print(dict)
 print("The sentence is: ", example)
 
 word_list =["The", "fire"]
